1f686395a6e545f1f9ae276650d5b541388c37ea
yurim3 <w2103@e-mirim.hs.kr>
2022-04-04 11:54:51 +0900

    좀 더 유연한 반복문

--- CodingTest/01 2차원배열.c
+++ CodingTest/01 2차원배열.c
@@ -5,13 +5,9 @@ int main(void)
 	int arr[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 	//1부터 12까지 출력해 보자.
 	
-	//반복 횟수를 12번 고정 (유연하지 못한 코드)
-	for (int i = 0; i < 12; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-
-	for (int i = 0; i < 12; i++)
+	//sizeof(arr) 배열 arr의 크기 : 4 * 12 = 48
+	//sizeof(int) int형 변수의 크기 : 4
+	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
 	{
 		printf("%d ", arr[i]);
 	}
