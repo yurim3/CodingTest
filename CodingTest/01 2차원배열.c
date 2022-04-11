7ed0bd37f2286c3dd673f3d72fe91e973a840d51
yurim3 <w2103@e-mirim.hs.kr>
2022-04-11 10:51:07 +0900

    이차원배열 살짝 유식하게

--- CodingTest/01 2차원배열.c
+++ CodingTest/01 2차원배열.c
@@ -21,4 +21,12 @@ int main(void)
 	printf("%d ", arr[2][2]);
 	printf("%d ", arr[2][3]);
 	printf("\n");
+
+	for (int i = 0; i < 3; i++) {
+		printf("%d ", arr[i][0]);
+		printf("%d ", arr[i][1]);
+		printf("%d ", arr[i][2]);
+		printf("%d ", arr[i][3]);
+		printf("\n");
+	}
 }
