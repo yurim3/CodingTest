99f352c9b978db36a6544bc3579ebd8797c3cd19
yurim3 <w2103@e-mirim.hs.kr>
2022-04-11 11:57:20 +0900

    최댓값 구하는 것을 유식하게

--- CodingTest/02 배열활용연습.c
+++ CodingTest/02 배열활용연습.c
@@ -9,48 +9,16 @@ int main(void)
 	int idx = 99; //인덱스 값
 	//배열 요소 중 최댓값과 그에 해당하는 idx값을 구하시오
 	
-	if (arr[0] > temp) {
-		temp = arr[0];
-		idx = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		if (arr[i] > temp)
+		{
+			temp = arr[i];
+			idx = i;
+		}
 	}
-	if (arr[1] > temp) {
-		temp = arr[1];
-		idx = 1;
-	}
-	if (arr[2] > temp) {
-		temp = arr[2];
-		idx = 2;
-	}
-	if (arr[3] > temp) {
-		temp = arr[3];
-		idx = 3;
-	}
-	if (arr[4] > temp) {
-		temp = arr[4];
-		idx = 4;
-	}
-	if (arr[5] > temp) {
-		temp = arr[5];
-		idx = 5;
-	}
-	if (arr[6] > temp) {
-		temp = arr[6];
-		idx = 6;
-	}
-	if (arr[7] > temp) {
-		temp = arr[7];
-		idx = 7;
-	}
-	if (arr[8] > temp) {
-		temp = arr[8];
-		idx = 8;
-	}
-	if (arr[9] > temp) {
-		temp = arr[9];
-		idx = 9;
-	}
+	
 	printf("값은 : %d 인덱스 값은 : %d \n", temp, idx);
 
-	for(int i = 0; i < arr[])
 	return 0;
 }
