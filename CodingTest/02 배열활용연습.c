a92d395275a89e32d2f751f5177f1344950b9f18
yurim3 <w2103@e-mirim.hs.kr>
2022-04-11 12:11:34 +0900

    최댓값 구하는 것을 함수화

--- CodingTest/02 배열활용연습.c
+++ CodingTest/02 배열활용연습.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-int main(void)
+void getMax(int* arr, int length) //배열은 포인터로 넘겨 줘야 함
 {
-	//최댓값은 14(arr[9])
-	int arr[10] = { 8, 5, 1, 6, 4, 9, 10, 7, 11, 14 };
-	
 	int temp = -1; //비교대상
 	int idx = 99; //인덱스 값
 	//배열 요소 중 최댓값과 그에 해당하는 idx값을 구하시오
-	
-	for (int i = 0; i < 10; i++)
+
+	for (int i = 0; i < length; i++)
 	{
 		if (arr[i] > temp)
 		{
@@ -17,8 +14,14 @@ int main(void)
 			idx = i;
 		}
 	}
-	
+
 	printf("값은 : %d 인덱스 값은 : %d \n", temp, idx);
+}
+int main(void)
+{
+	//최댓값은 14(arr[9])
+	int arr1[10] = { 8, 5, 1, 6, 4, 9, 10, 7, 11, 14 };
+	getMax(arr1, 10);
 
 	return 0;
 }
