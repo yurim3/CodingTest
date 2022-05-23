0724d0930870ef47cbbe530ea39bfe038891cc5d
yurim3 <w2103@e-mirim.hs.kr>
2022-05-23 11:12:38 +0900

    문자열 복사 유연하게

--- CodingTest/04 문자열 복사하기.c
+++ CodingTest/04 문자열 복사하기.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <string.h>		//strlen() 호출
 
 int main(void)
 {
 	//src에 있는 문자열이 내용을 dest에 복사하세요
 	char dest[20]; //복사할 공간
 	char src[] = "Hello"; //복사할 문자열
+	int length = strlne(src); 
+
+	//for(int i = 0; src[i] != '\0'; i++)
+	for (int i = 0; i < length; i++) {
+		dest[i] = src[i];
+	}
 	for (int i = 0; i < 6; i++) {
 		dest[i] = src[i];
 	}
