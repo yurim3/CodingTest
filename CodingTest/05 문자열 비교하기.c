e7ed4e190284236f69c4a67f45b2d3c63e1b9267
yurim3 <w2103@e-mirim.hs.kr>
2022-05-30 11:05:34 +0900

    문자열 비교하기

---
+++ CodingTest/05 문자열 비교하기.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>		//strlen() 호출
+
+//두 문자열의 내용이 같은지 다른지를 확인
+//같으면 0을 반환, 다르면 1을 반환
+int my_strcmp(char* str1, char* str2)
+{
+	int str1_len = strlen(str1); //str1의 문자 개수
+	int str2_len = strlen(str2); //str2의 문자 개수
+
+	//두 문자열의 길이가 다른 경우
+	if (str1_len != str2_len)
+		return 1;
+
+	//하나라도 글자가 다른 경우
+	for (int i = 0; i < str1_len; i++)
+	{
+		if (str1[i] != str2[i])
+			return 1;
+	}
+}
+
+int main(void)
+{
+	char a[20] = "Hello";
+	char b[20] = "Yello";
+	my_strcmp(a, b);
+	return 0;
+}
