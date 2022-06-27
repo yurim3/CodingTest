cbf39078be79fc283d4b5a5c8542999896200b08
yurim3 <w2103@e-mirim.hs.kr>
2022-06-27 11:44:19 +0900

    문제 풀어보기

--- CodingTest/1학기 수행평가 대비.c
+++ CodingTest/1학기 수행평가 대비.c
@@ -23,6 +23,31 @@ int main(void) {
 		printf("양서영입니다. \n");
 	}
 	else {
-		printf("양서영이 아닙니다. \n");
+		printf("양서영이 아닙니다. \n\n");
+	}
+
+
+
+
+	//문제1 문자열의 길이 구하기
+	char sstr[30] = "samgyetang";
+	int llength = strlen(sstr);
+	printf("삼계탕의 길이는 %d\n", llength);
+
+	//문제2 copy_sstr에 "iceshu" 집어넣기(복사하기)
+	char copy_sstr[30];
+	strcpy(copy_sstr, "iceshu");
+	printf("복사한 문자열은 %s \n", copy_sstr);
+
+	//문제3 copy_sstr에 "delicious" 덧붙이기
+	strcat(copy_sstr, "delicious");
+	printf("덧붙인 문자열은 %s \n", copy_sstr);
+
+	//문제4 "BMW"와 "benz"의 두 문자열이 같은지 다른지를 판별
+	if(strcmp("BMW", "benz") == 0) {
+		printf("두 문자열은 같습니다.");
+	}
+	else {
+		printf("두 문자열은 같지 않습니다.");
 	}
 }
