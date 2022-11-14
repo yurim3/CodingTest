11c6ad87bacc158c87937cedc95769bef0b8ba2d
yurim3 <w2103@e-mirim.hs.kr>
2022-11-14 13:11:08 +0900

    cos pro 기출문제 4 코드(왕 유연하게)

--- CodingTest/03-04.c
+++ CodingTest/03-04.c
@@ -5,32 +5,13 @@
 int solution(char* words[], int words_len, char* word) {
     int count = 0;
     // words[0] : "CODE", words[1] : "COED", words[2] : "CDEO"
-    if (word[0] != words[0][0])
-        count++;
-    if (word[1] != words[0][1])
-        count++;
-    if (word[2] != words[0][2])
-        count++;
-    if (word[3] != words[0][3])
-        count++;
-
-    if (word[0] != words[1][0])
-        count++;
-    if (word[1] != words[1][1])
-        count++;
-    if (word[2] != words[1][2])
-        count++;
-    if (word[3] != words[1][3])
-        count++;
-
-    if (word[0] != words[2][0])
-        count++;
-    if (word[1] != words[2][1])
-        count++;
-    if (word[2] != words[2][2])
-        count++;
-    if (word[3] != words[2][3])
-        count++;
+    for (int i = 0; i < words_len; i++) {
+        for (int j = 0; j < strlen(word); j++) {
+            if (word[j] != words[i][j]) {
+                count++;
+            }
+        }
+    }
     return count;
 }
 
