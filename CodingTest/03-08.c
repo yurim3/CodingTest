fa9f566206ef42a3ebd22ddea389f3d31df92bfe
yurim3 <w2103@e-mirim.hs.kr>
2022-11-21 12:04:10 +0900

    cos pro 기출문제 8 코드 완성

--- CodingTest/03-08.c
+++ CodingTest/03-08.c
@@ -2,17 +2,19 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-
+// tv의 개수(= programs_len)
 int solution(int programs[][2], int programs_len) {
     int answer = 0;
     int used_tv[25] = { 0, };
 
+    // 시간대별 방영한 tv의 개수를 count
     for (int i = 0; i < programs_len; i++)
-        for (int j = programs[i][0]; j < programs[i][1]; j++)
+        for (int j = programs[i][0]; j < programs[i][1]; j++) //programs[i][0] : 시작 시간, programs[i][1] : 끝나는 시간
             used_tv[j]++;
 
+    // 하루에 tv를 2대 이상 트는 총 시간을 return하도록
     for (int i = 0; i < 25; i++)
-        if (used_tv[i] >= 1)
+        if (used_tv[i] >= 2)
             answer++;
 
     return answer;
